fee9edcbe262c780f5958733235b71c8b6b1086e
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2025-12-01 09:32:57 +0100

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,23 +1,22 @@
-// https://www.youtube.com/watch?v=Ck-CoNNn89g&ab_channel=NHNTV
 import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
     title: 'Users API',
     version: "1.0.0",
-    description: 'Users API'
+    description: 'Users API swagger page'
   },
   tags: [
     {
-      name: 'API',
-      description: 'API endpoints'
+      name: 'API swagger',
+      description: 'Endpoints'
     }
   ],
   host: 'localhost:3000',
   basePath: "/api/users"
 };
 
-const outputFile = './swagger-output.json';
+const outputFile = './swagger-docs.json';
 const routes = ['./routes/users.js'];
 
 swaggerAutogen()(outputFile, routes, doc);
